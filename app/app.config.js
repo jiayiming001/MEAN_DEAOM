12b05da8617bcc3ade4d6002a5c3505bb5bef7b8
jiayiming <qq1220102512@outlook.com>
2018-05-23 22:47:31 +0800

    cookie  session 使用

--- app/app.config.js
+++ app/app.config.js
@@ -1,5 +1,4 @@
 'use strict';
-
 angular.
     module('myTest').
     config(['$locationProvider','$routeProvider',
--- service/routes/index.server.routes.js
+++ service/routes/index.server.routes.js
@@ -1,18 +1,30 @@
 const path = require("path");
 module.exports = function(app) {
    // var index = require('../constrollers/index.server.constroller');
-    app.use(function (req, res) {  //angular使用了html5Model,路由没有了#,后端过滤后把路由给前端
+    app.use(function (req, res, next) {  //angular使用了html5Model,路由没有了#,后端过滤后把路由给前端
         if(req.path.indexOf("/api")>=0) {
             res.send("API server");
         } else {
-            if(req.session['count'] == null) {
-                req.session['count'] = 1; 
-            }else {
-                req.session['count']++;
-            }
-            console.log(req.session['count'])
-            res.sendFile(path.join(__dirname + '/../views/index.html'));
+            next('route');
         }
     });
+
+    app.get('/login', function (req, res, next) {
+        if (req.session.isFirst) {
+            console.log('欢迎再次访问');
+            console.log(req.session.isFirst);
+        } else {
+            req.session.isFirst = { 'username': '123' }
+            console.log('欢迎第一次访问');
+        }
+        res.sendFile(path.join(__dirname + '/../views/index.html'));
+    });
+    
+    app.get('/register', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../views/index.html'));
+    })
      
+    app.get('/', function (req, res) {
+        res.redirect('/login');
+    })
 };
